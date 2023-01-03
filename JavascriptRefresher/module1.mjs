6d6d2b532a21716d218a900b113714d67b20f32d
ViurAgarwal25 <geurecruitviduragarwal@gmail.com>
2023-01-04 02:33:10 +0530

    export default vs export

---
+++ JavascriptRefresher/module1.mjs
@@ -0,0 +1,8 @@
+let a = "vidur";
+let b = "agarwal";
+let c = "meerut";
+export default a;
+export { b };
+//export default is used and then we can import in another file
+//export is used alone when apart from default we still have to export something from our modules
+// if we are exporting b make sure there also we are using name vairbale b while importing
---
+++ JavascriptRefresher/module2.mjs
@@ -0,0 +1,3 @@
+import name, { b } from "./module1.mjs";
+console.log(name);
+console.log(b);
