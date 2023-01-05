9957e156106add748c4be43ee6b455a74ab4763f
ViurAgarwal25 <geurecruitviduragarwal@gmail.com>
2023-01-05 17:45:22 +0530

    Added feature like word count and characters and case conversion

--- my-app/src/components/Textform.js
+++ my-app/src/components/Textform.js
@@ -7,25 +7,58 @@ export default function Textform(props) {
     var newtext = text.toUpperCase();
     setText(newtext);
   };
+  const handleLowerCaseClick = () => {
+    setText(text.toLowerCase());
+  };
   const handleOnChange = (event) => {
     console.log("function handleonchange is called");
     setText(event.target.value);
   };
+  const handleCapitalizedCaseClick = () => {
+    var arr = text.split(" ");
+    for (var i = 0; i < arr.length; i++) {
+      arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+    var newtext = arr.join(" ");
+    setText(newtext);
+  };
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="myBox"
-          rows="8"
-        ></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpperCaseClick}>
+          CONVERT TO UPPERCASE
+        </button>
+        <button className="btn btn-success mx-1" onClick={handleLowerCaseClick}>
+          convert to lowercase
+        </button>
+        <button
+          className="btn btn-success mx-1"
+          onClick={handleCapitalizedCaseClick}
+        >
+          Convert To Capitalized Case
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpperCaseClick}>
-        CONVERT TO UPPERCASE
-      </button>
-    </div>
+      <div className="container my-3">
+        <h1>Your text summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <h3>Average Reading time for this (125 words/minute):-</h3>
+
+        <p>{(1 / 125) * text.split(" ").length}</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
